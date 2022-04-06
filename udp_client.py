61d69992eb2419a93153dad2a47af01776fa5e3b
Godfred Doe <godfredoe9@gmail.com>
2022-04-06 10:27:09 +0000

    Modified

--- udp_client.py
+++ udp_client.py
@@ -6,11 +6,12 @@ buff_size = 1024
 
 udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-client_msg = bytes("Hello UDP Server", encoding='utf-8')
+client_msg = bytes(input("Message: "), encoding='utf-8')
 udp_client.sendto(client_msg, (host, port))
 print(f"Message sent to {(host, port)}")
 
 server_msg, server_addr = udp_client.recvfrom(buff_size)
+server_msg = server_msg.decode('utf-8')
 
 print(f"Message received from {(host, port)}")
-print(f"{server_addr}: server_msg")
+print(f"{server_addr}: {server_msg}")
--- udp_server.py
+++ udp_server.py
@@ -6,12 +6,14 @@ buffer_size = 1024
 
 udp_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_server.bind((host, port))
-print(f"Listening on {(host, port)}")
+print(f"Server bound to {(host, port)}")
 
-while True:
-    client_msg, client_addr = udp_server.recvfrom(1024)
-    print(f"Client {client_addr} sent a message")
-    print(f"{client_addr}: client_msg")
+with udp_server:
+    while True:
+        client_msg, client_addr = udp_server.recvfrom(buffer_size)
+        print(f"Client {client_addr} sent a message")
+        print(f"{client_addr}: {client_msg!r}")
 
-    server_msg = bytes("Hello from the server", encoding='utf-8')
-    udp_server.sendto(server_msg, client_addr)
+        server_msg = bytes(input("Message: "), encoding='utf-8')
+        udp_server.sendto(server_msg, client_addr)
+        break
